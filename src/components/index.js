8660f6b2c8115a674082b9d6ca3aa9c77f9d9169
Tawyyib <105920744+Tawyyib@users.noreply.github.com>
2023-11-08 10:09:21 +0100

    project files upload
    First time upload of all project files

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Onboard } from './onboard/Onboard';
+export { default as Preloader } from './preloader/Preloader';
+

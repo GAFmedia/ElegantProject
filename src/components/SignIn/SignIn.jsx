bb45dad1f3ff1303a5b1254ca49cf1368d392b86
HadiBRQ <hadiibrahim776@gmail.com>
2023-11-14 08:16:56 +0100

    folder name update

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //import { useState } from "react";
-import "./style.scss";
+import "./signin.scss";
 
 //const [rememberPasswordCheckbox, changeCheckboxStatus] = useState("rectangle");
 

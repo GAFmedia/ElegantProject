ac51bd240d84eb81facb3ea804c55e7a10b1a49a
Tawyyib <105920744+Tawyyib@users.noreply.github.com>
2023-11-17 20:41:15 +0100

    component items updated
    Component Items added onto export index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
-export { default as Onboard } from './onboard/Onboard';
 export { default as Preloader } from './preloader/Preloader';
+export { default as OnboardCarousel } from './onboardCarousel/OnboardCarousel';
+export { default as WideButton } from './wideButton/WideButton';
 export { default as SignInForm } from './signin/SignInForm';
 

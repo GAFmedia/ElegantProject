75c5c3851c1543c57849f02bc0eabff9f3781438
Tawyyib <105920744+Tawyyib@users.noreply.github.com>
2023-11-17 20:46:51 +0100

    New Item added
    Onboarding ID added onto container index

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,5 @@
 
 export { default as Header } from './header/Header';
+export { default as Onboarding } from './onboarding/Onboarding';
 export { default as Footer } from './footer/Footer';
 export { default as Starter } from './starter/Starter';

f774334f0912bb930b13b08bceafeca1a7703904
Tawyyib <105920744+Tawyyib@users.noreply.github.com>
2023-11-20 14:38:00 +0100

    File content clean up and updated
    File Content cleaned up of unwanted items

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,10 @@
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Preloader } from './preloader/Preloader';
-export { default as OnboardCarousel } from './onboardCarousel/OnboardCarousel';
-export { default as WideButton } from './wideButton/WideButton';
-export { default as SignInForm } from './signin/SignInForm';
+export { default as OnboardingCarousel } from './onboardingCarousel/OnboardingCarousel';
+export { default as AppButton } from './appButton/AppButton';
+export { default as OnButton } from './appButton/OnButton';
+export { default as SignIn } from './signin/SignIn';
+export { default as AuthenticateModal } from './authenticateModal/AuthenticateModal';
+//export { default as LoginSelect } from './loginselect/LoginSelect';
 
